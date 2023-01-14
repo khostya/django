f88d2e5ba49c074279c0dd1af097473fae71557f
khostya <fgsryttt@gmail.com>
2023-01-14 14:15:19 +0500

    получение вакансий по дате

--- static/js/last-vacancies.js
+++ static/js/last-vacancies.js
@@ -1,25 +1,34 @@
-let vacanciesQuery = 'https://api.hh.ru/vacancies?per_page=10&page=0&order_by=publication_time&text='
-let names = ['engineer', 'инженер+программист', 'інженер', 'it+инженер', 'инженер+разработчик']
-let promises = []
-for (name of names){
-    let query = vacanciesQuery + name
-    promises.push(fetch(query)
-        .then(r => r.json())
-        .then(x => x.items))
-}
+updateVacancies(new Date())
 
-Promise.all(promises).then(vacancies =>{
-    let listVacancies = []
-    for (let vacancy of vacancies){
-        listVacancies = listVacancies.concat(vacancy)
-    }
+function updateVacancies(date) {
+    date = date.toISOString().split("T")[0]
+    let vacanciesQuery = 'https://api.hh.ru/vacancies?per_page=10&' +
+        'page=0&order_by=publication_time&date_from='+ date +'&date_to=' + date + '&text=';
+    let names = ['engineer', 'инженер+программист', 'інженер', 'it+инженер', 'инженер+разработчик']
     let promises = []
-    listVacancies.sort((a, b) => new Date(a).getMilliseconds() - new Date(b).getMilliseconds())
-    for (let i = 0; i < 10; i++){
-        promises.push(fetch('https://api.hh.ru/vacancies' + '/' + listVacancies[i]['id']).then(r => r.json()))
+    for (name of names){
+        let query = vacanciesQuery + name
+        promises.push(fetch(query)
+            .then(r => r.json())
+            .then(x => x.items))
     }
-    Promise.all(promises).then(updateTable)
-})
+    let tbody = document.getElementsByTagName('tbody')
+    while (tbody[0].rows[0]){
+        tbody[0].deleteRow(0)
+    }
+    Promise.all(promises).then(vacancies =>{
+        let listVacancies = []
+        for (let vacancy of vacancies){
+            listVacancies = listVacancies.concat(vacancy)
+        }
+        let promises = []
+        listVacancies.sort((a, b) => new Date(a).getMilliseconds() - new Date(b).getMilliseconds())
+        for (let i = 0; i < 10; i++){
+            promises.push(fetch('https://api.hh.ru/vacancies' + '/' + listVacancies[i]['id']).then(r => r.json()))
+        }
+        Promise.all(promises).then(updateTable)
+    })
+}
 
 function extractDescription(s) {
     function removeTags(s) {
@@ -39,7 +48,6 @@ function extractKeySkills(keySkillsJSON){
 }
 
 function extractSalary(salaryJSON){
-
     if (salaryJSON == null){
         return 'не указано'
     }
@@ -53,7 +61,6 @@ function updateTable(vacancies){
     let tbody = vacancyTable.getElementsByTagName("tbody")[0]
     for (let i = 0; i < 10; i++){
         let newRow = tbody.insertRow()
-        console.log(vacancies.at(0))
         newRow.insertCell().appendChild(document.createTextNode(vacancies[i]['name']))
         newRow.insertCell().appendChild(document.createTextNode(extractDescription(vacancies[i]['description'])))
         newRow.insertCell().appendChild(document.createTextNode(extractKeySkills(vacancies[i]['key_skills'])))
